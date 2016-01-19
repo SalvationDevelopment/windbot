c2b5221cb3cac55069e84d8a5542b3841acca526
Zayelion <panthrowzay@gmail.com>
2016-01-19 03:23:58 -0500

    Fix

--- Program.cs
+++ Program.cs
@@ -5,27 +5,28 @@ using System.Threading;
 using WindBot.Game;
 using WindBot.Game.AI;
 
+
 namespace WindBot
 {
     public class Program
     {
         public const short ProVersion = 0x1338;
 
-        internal static Random Rand;
-        
+        public static Random Rand;
+
         public static void Main(string[] args)
         {
 #if !DEBUG
             try
             {
-                private static void Run(String username, String deck, String serverIP, int serverPort,String password);
+                Run(args[0], args[1], args[2], int.Parse(args[3]), args[4]);
             }
             catch (Exception ex)
             {
                 Console.Error.WriteLine("Error: " + ex);
             }
 #else
-           private static void Run(String username, String deck, String serverIP, int serverPort,String password);
+            Run(args[0], args[1], args[2], int.Parse(args[3]), args[4]);
 #endif
         }
 
@@ -36,7 +37,7 @@ namespace WindBot
             InitCardsManager(databasePath);
         }
         
-        private static void Run()
+        private static void Run(String username, String deck, String serverIP, int serverPort, String password)
         {
             Init("cards.cdb");
 
@@ -44,7 +45,7 @@ namespace WindBot
             GameClient clientA = new GameClient(username, deck, serverIP, serverPort, password);
             clientA.Start();
            
-            while (clientA.Connection.IsConnected || clientB.Connection.IsConnected)
+            while (clientA.Connection.IsConnected)
             {
                 clientA.Tick();
                 Thread.Sleep(1);
